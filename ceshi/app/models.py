7c22b266574aac6fe6608d7b038c90c8174b556a
kog <366986211@qq.com>
2018-10-09 16:54:03 +0800

    django

--- ceshi/app/models.py
+++ ceshi/app/models.py
@@ -1,3 +1,8 @@
 from django.db import models
 
 # Create your models here.
+
+
+class students(models.Model):
+    name = models.CharField(max_length=20)
+    age = models.IntegerField(max_length=20,default=18)
--- ceshi/ceshi/settings.py
+++ ceshi/ceshi/settings.py
@@ -77,8 +77,15 @@ WSGI_APPLICATION = 'ceshi.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        # 'ENGINE': 'django.db.backends.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        # 'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'NAME': 'test',
+        'USER': 'root',
+        'PASSWORD': '12345678',
+        'HOST': '127.0.0.1',
+        'PORT': '3306',
+
     }
 }
 
