c24390af6bb208c97022f409801cfcaa7dbb62af
kogwang <365986211@qq.com>
2019-03-13 11:27:13 +0800

    测试使用

--- ceshi/app/models.py
+++
@@ -1,8 +0,0 @@
-from django.db import models
-
-# Create your models here.
-
-
-class students(models.Model):
-    name = models.CharField(max_length=20)
-    age = models.IntegerField(max_length=20,default=18)
--- mytest/mytest/urls.py
+++ mytest/mytest/urls.py
@@ -1,4 +1,4 @@
-"""ceshi URL Configuration
+"""mytest URL Configuration
 
 The `urlpatterns` list routes URLs to views. For more information please see:
     https://docs.djangoproject.com/en/2.1/topics/http/urls/
@@ -15,8 +15,7 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
-from app import views
+
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('api/', views.hahaha),
 ]
